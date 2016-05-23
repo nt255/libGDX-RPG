b84602f88bd5c7cc4f210e214107719749766978
ntuntasood <ntuntasood@gmail.com>
2016-05-22 21:22:17 -0700

    removed badlogic.jpg, added tilemap rendering

--- core/src/com/gdxrpg/game/Game.java
+++ core/src/com/gdxrpg/game/Game.java
@@ -3,25 +3,38 @@ package com.gdxrpg.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
 public class Game extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+
+	private static final String CAFE_MAP = "cafe.tmx";
+
+	private TiledMapRenderer mapRenderer;
+	private OrthographicCamera camera;
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		TiledMap m = new TmxMapLoader().load(CAFE_MAP);
+		mapRenderer = new OrthogonalTiledMapRenderer(m);
+
+		int w = Gdx.graphics.getWidth();
+		int h = Gdx.graphics.getHeight();
+
+		camera = new OrthographicCamera(w, h);
+		camera.translate(w/2f, h/2f);
+		camera.update();
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		mapRenderer.setView(camera);
+		mapRenderer.render();
 	}
+
 }
