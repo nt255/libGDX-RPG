c5d4512207e602b02aa69f86c6bfe168071fd55a
ntuntasood <ntuntasood@gmail.com>
2016-05-24 21:23:26 -0700

    moved tilemap rendering code into new class

--- core/src/com/gdxrpg/game/Game.java
+++ core/src/com/gdxrpg/game/Game.java
@@ -4,28 +4,8 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputProcessor;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
-import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
-import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
-import com.badlogic.gdx.maps.tiled.TmxMapLoader;
-import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
 public class Game extends ApplicationAdapter implements InputProcessor {
-
-	private static final String CAFE_MAP = "cafe.tmx";
-
-	private static final float BG_COLOR_RED   = 0.1f;
-	private static final float BG_COLOR_GREEN = 0.1f;
-	private static final float BG_COLOR_BLUE  = 0.1f;
-	private static final float BG_COLOR_ALPHA = 1.0f;
-
-	private static final float CROP_PX_SIDE   = 23f;
-	private static final float CROP_PX_TOP    = 17f;
-	private static final float CROP_PX_BOTTOM = 16f;
 
 	private boolean facingDown = true; // faces down by default
 	private boolean facingLeft = false;
@@ -37,57 +17,19 @@ public class Game extends ApplicationAdapter implements InputProcessor {
 	private boolean movingRight = false;
 	private boolean movingUp = false;
 
-	private float mapWidth;
-	private float mapHeight;
-
-	private OrthographicCamera camera;
-	private TiledMapRenderer mapRenderer;
-	private ShapeRenderer shapeRenderer;
-
+	private GameMapRenderer mapRenderer;
 	private MainCharacter mainCharacter;
 
 	@Override
 	public void create () {
-		TiledMap m = new TmxMapLoader().load(CAFE_MAP);
-		TiledMapTileLayer l = (TiledMapTileLayer) m.getLayers().get(0);
-
-		mapWidth = l.getWidth() * l.getTileWidth();
-		mapHeight = l.getHeight() * l.getTileHeight();
-
-		int w = Gdx.graphics.getWidth();
-		int h = Gdx.graphics.getHeight();
-
-		camera = new OrthographicCamera(w, h);
-		camera.translate(mapWidth/2, mapHeight/2);
-		camera.update();
-
-		mapRenderer = new OrthogonalTiledMapRenderer(m);
-		mapRenderer.setView(camera);
-
-		shapeRenderer = new ShapeRenderer();
-		shapeRenderer.setProjectionMatrix(camera.combined);
-		shapeRenderer.setColor(BG_COLOR_RED, BG_COLOR_GREEN,
-				BG_COLOR_BLUE, BG_COLOR_ALPHA);
-
+		mapRenderer = new GameMapRenderer();
 		mainCharacter = new MainCharacter();
-
 		Gdx.input.setInputProcessor(this);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(BG_COLOR_RED, BG_COLOR_GREEN,
-				BG_COLOR_BLUE, BG_COLOR_ALPHA);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
 		mapRenderer.render();
-
-		shapeRenderer.begin(ShapeType.Filled);
-		shapeRenderer.rect(0, 0, CROP_PX_SIDE, mapHeight);
-		shapeRenderer.rect(0, 0, mapWidth, CROP_PX_BOTTOM);
-		shapeRenderer.rect(mapWidth, 0, -CROP_PX_SIDE, mapHeight);
-		shapeRenderer.rect(0, mapHeight, mapWidth, -CROP_PX_TOP);
-		shapeRenderer.end();
 
 		if (facingDown)
 			mainCharacter.faceDown();
@@ -123,6 +65,7 @@ public class Game extends ApplicationAdapter implements InputProcessor {
 			movingRight = true;
 		if (keycode == Input.Keys.UP)
 			movingUp = true;
+
 		return false;
 	}
 
@@ -141,6 +84,7 @@ public class Game extends ApplicationAdapter implements InputProcessor {
 		movingLeft = false;
 		movingRight = false;
 		movingUp = false;
+
 		return false;
 	}
 
---
+++ core/src/com/gdxrpg/game/GameMapRenderer.java
@@ -0,0 +1,72 @@
+package com.gdxrpg.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+
+public class GameMapRenderer {
+
+	private static final String CAFE_MAP = "cafe.tmx";
+
+	private static final float BG_COLOR_RED   = 0.1f;
+	private static final float BG_COLOR_GREEN = 0.1f;
+	private static final float BG_COLOR_BLUE  = 0.1f;
+	private static final float BG_COLOR_ALPHA = 1.0f;
+
+	private static final float CROP_PX_SIDE   = 23f;
+	private static final float CROP_PX_TOP    = 17f;
+	private static final float CROP_PX_BOTTOM = 16f;
+
+	private float mapWidth;
+	private float mapHeight;
+
+	private OrthographicCamera camera;
+	private TiledMapRenderer mapRenderer;
+	private ShapeRenderer shapeRenderer;
+
+	public GameMapRenderer() {
+		TiledMap m = new TmxMapLoader().load(CAFE_MAP);
+		TiledMapTileLayer l = (TiledMapTileLayer) m.getLayers().get(0);
+
+		mapWidth = l.getWidth() * l.getTileWidth();
+		mapHeight = l.getHeight() * l.getTileHeight();
+
+		int w = Gdx.graphics.getWidth();
+		int h = Gdx.graphics.getHeight();
+
+		camera = new OrthographicCamera(w, h);
+		camera.translate(mapWidth/2, mapHeight/2);
+		camera.update();
+
+		mapRenderer = new OrthogonalTiledMapRenderer(m);
+		mapRenderer.setView(camera);
+
+		shapeRenderer = new ShapeRenderer();
+		shapeRenderer.setProjectionMatrix(camera.combined);
+		shapeRenderer.setColor(BG_COLOR_RED, BG_COLOR_GREEN,
+				BG_COLOR_BLUE, BG_COLOR_ALPHA);
+	}
+
+	public void render() {
+		Gdx.gl.glClearColor(BG_COLOR_RED, BG_COLOR_GREEN,
+				BG_COLOR_BLUE, BG_COLOR_ALPHA);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		mapRenderer.render();
+
+		shapeRenderer.begin(ShapeType.Filled);
+		shapeRenderer.rect(0, 0, CROP_PX_SIDE, mapHeight);
+		shapeRenderer.rect(0, 0, mapWidth, CROP_PX_BOTTOM);
+		shapeRenderer.rect(mapWidth, 0, -CROP_PX_SIDE, mapHeight);
+		shapeRenderer.rect(0, mapHeight, mapWidth, -CROP_PX_TOP);
+		shapeRenderer.end();
+	}
+
+}
